2b86f8298f996a843d7874a7b9acd4130e99e7c8
Kyle Evans <evans.584@osu.edu>
2018-05-15 15:36:17 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'kbe_nginx'
+maintainer 'Kyle Evans'
+maintainer_email 'kylebe@gmail.com'
+license 'all_rights'
+description 'Installs/Configures kbe_nginx'
+long_description 'Installs/Configures kbe_nginx'
+version '0.1.0'
